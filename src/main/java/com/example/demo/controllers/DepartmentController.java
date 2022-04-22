b5aaed92e7e75e14d49021cd62e34148ffa1b963
Eliot996 <math10t6@stud.kea.dk>
2022-04-22 10:48:34 +0200

    made work

--- src/main/java/com/example/demo/controllers/DepartmentController.java
+++ src/main/java/com/example/demo/controllers/DepartmentController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Department;
-import com.example.demo.repositories.CRUDInterface;
+import com.example.demo.repositories.IRepository;
 import com.example.demo.repositories.DepartmentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DepartmentController {
-    private CRUDInterface<Department> departmentRepository = new DepartmentRepository();
+    private IRepository<Department> departmentRepository = new DepartmentRepository();
 
     @GetMapping("/departments")
     public String allDepartments(Model model){
+        model.addAttribute("departments", departmentRepository.getAllEntities());
         return "departments";
     }
 }
--- src/main/java/com/example/demo/repositories/DepartmentRepository.java
+++ src/main/java/com/example/demo/repositories/DepartmentRepository.java
@@ -2,14 +2,13 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Department;
 import com.example.demo.utility.DatabaseConnectionManager;
-import com.mysql.cj.protocol.Resultset;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class  DepartmentRepository implements CRUDInterface<Department>{
+public class  DepartmentRepository implements IRepository<Department> {
     @Override
     public boolean create(Department entity) {
         return false;
@@ -24,15 +23,27 @@ public class  DepartmentRepository implements CRUDInterface<Department>{
     public List<Department> getAllEntities() {
         try {
             Connection conn = DatabaseConnectionManager.getConnection();
-            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM departments");
-            ResultSet rs = stmt.executeQuery();
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM departments");
+
+            ResultSet rs = pstmt.executeQuery();
+
+            ArrayList<Department> allDepartments = new ArrayList<>();
             while(rs.next()){
                 //Indsætte i en liste
+                Department temp = new Department(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3)
+                );
+                allDepartments.add(temp);
             }
+
+            return allDepartments;
         }
         catch(SQLException e){
             e.printStackTrace();
             System.out.println("Something wrong with database");
+            e.printStackTrace();
         }
         return null;
     }
