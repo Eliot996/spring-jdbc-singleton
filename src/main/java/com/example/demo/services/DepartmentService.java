77c220a4fb31fa066c75f2a156e2b145d612a54f
Eliot996 <math10t6@stud.kea.dk>
2022-04-26 17:00:09 +0200

    mostEmployees working and passed test

--- src/main/java/com/example/demo/services/DepartmentService.java
+++ src/main/java/com/example/demo/services/DepartmentService.java
@@ -1,13 +1,17 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Department;
+import com.example.demo.models.Employee;
 import com.example.demo.repositories.DepartmentRepository;
+import com.example.demo.repositories.EmployeeRepository;
 import com.example.demo.repositories.IRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentService {
     IRepository<Department> repo = new DepartmentRepository();
+    IRepository<Employee> employeeRepo = new EmployeeRepository();
 
     public int getHighestDepartmentNumber() {
         List<Department> list = repo.getAllEntities();
@@ -22,7 +26,41 @@ public class DepartmentService {
         return highest;
     }
 
+    public int getDepartmentNumberWithMostEmployees() {
+        // run through the list of employees and add to a hashmap<DepartmentNumber, amount> or use double Arraylist
+        // return the department with the most employees.
+
+        List<Employee> list = employeeRepo.getAllEntities();
+        ArrayList<Integer> departments = new ArrayList<>();
+        ArrayList<Integer> amount = new ArrayList<>();
+
+        int dep_num;
+        for (Employee e : list) {
+            dep_num = e.getDepartment_number();
+            if (departments.contains(dep_num)) {
+                amount.set(departments.indexOf(dep_num), amount.get(departments.indexOf(dep_num)) + 1);
+            } else {
+                departments.add(dep_num);
+                amount.add(1);
+            }
+        }
+
+        int mostEmployees = 0;
+
+        for (int i : amount) {
+            if (i > mostEmployees) {
+                mostEmployees = i;
+            }
+        }
+
+        return departments.get(amount.indexOf(mostEmployees));
+    }
+
     public void setRepo(IRepository<Department> repo) {
         this.repo = repo;
+    }
+
+    public void setEmployeeRepo(IRepository<Employee> employeeRepo) {
+        this.employeeRepo = employeeRepo;
     }
 }
--- src/test/java/com/example/demo/services/DepartmentServiceTest.java
+++ src/test/java/com/example/demo/services/DepartmentServiceTest.java
@@ -1,24 +1,37 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Department;
+import com.example.demo.models.Employee;
 import com.example.demo.repositories.IRepository;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentServiceTest {
 
     private static final IRepository<Department> MDR = new MockDepartmentRepo();
+    private static final MockEmployeeRepo MER = new MockEmployeeRepo();
     private static final DepartmentService DS = new DepartmentService();
 
     @BeforeAll
     public static void prep() {
+        // create mock employees
+        MER.create(new Employee(0, 0, 40, 0, 8, new Date(), "name", "job"));
+        MER.create(new Employee(1, 0, 40, 0, 9, new Date(), "name", "job"));
+        MER.create(new Employee(2, 0, 40, 0, 10, new Date(), "name", "job"));
+        MER.create(new Employee(3, 0, 50, 0, 11, new Date(), "name", "job"));
+        MER.create(new Employee(4, 0, 50, 0, 12, new Date(), "name", "job"));
+
+        // create mock departments
         MDR.create(new Department(40, "name", "location"));
         MDR.create(new Department(50, "name", "location"));
 
         DS.setRepo(MDR);
+        DS.setEmployeeRepo(MER);
     }
 
     @Test
@@ -28,6 +41,18 @@ class DepartmentServiceTest {
 
         // Act
         int actual = DS.getHighestDepartmentNumber();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getDepartmentNumberWithMostEmployees() {
+        // Arrange
+        int expected = 40;
+
+        // Act
+        int actual = DS.getDepartmentNumberWithMostEmployees();
 
         // Assert
         assertEquals(expected, actual);
