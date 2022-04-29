19ffc657e3bb1e0fac8f4a196b2138c491404584
Eliot996 <math10t6@stud.kea.dk>
2022-04-29 10:37:32 +0200

    refactored getDepartmentNumberWithMostEmployees() to use tuples

--- src/main/java/com/example/demo/services/DepartmentService.java
+++ src/main/java/com/example/demo/services/DepartmentService.java
@@ -5,6 +5,8 @@ import com.example.demo.models.Employee;
 import com.example.demo.repositories.DepartmentRepository;
 import com.example.demo.repositories.EmployeeRepository;
 import com.example.demo.repositories.IRepository;
+import org.javatuples.Pair;
+import org.javatuples.Triplet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,29 +30,34 @@ public class DepartmentService {
 
     public int getDepartmentNumberWithMostEmployees() {
         List<Employee> list = employeeRepo.getAllEntities();
-        ArrayList<Integer> departments = new ArrayList<>();
-        ArrayList<Integer> amount = new ArrayList<>();
+
+        // make into tuples
+        ArrayList<Pair<Integer, Integer>> departments = new ArrayList<>();
 
         int dep_num;
+        int index;
+        Pair<Integer, Integer> tempPair;
+
         for (Employee e : list) {
             dep_num = e.getDepartment_number();
-            if (departments.contains(dep_num)) {
-                amount.set(departments.indexOf(dep_num), amount.get(departments.indexOf(dep_num)) + 1);
+            index = contains(departments, dep_num);
+
+            if (index == -1) {
+                departments.add(new Pair<>(dep_num, 1));
             } else {
-                departments.add(dep_num);
-                amount.add(1);
+                tempPair = departments.get(index);
+                tempPair.setAt1(tempPair.getValue1() + 1);
             }
         }
 
-        int mostEmployees = 0;
-
-        for (int i : amount) {
-            if (i > mostEmployees) {
-                mostEmployees = i;
+        tempPair = new Pair<>(0,0);
+        for (Pair<Integer, Integer> p : departments) {
+            if (tempPair.getValue1() < p.getValue1()) {
+                tempPair = p;
             }
         }
 
-        return departments.get(amount.indexOf(mostEmployees));
+        return tempPair.getValue0();
     }
 
     public int getDepartmentNumberWithHighestAverageSalary() {
@@ -93,5 +100,27 @@ public class DepartmentService {
 
     public void setEmployeeRepo(IRepository<Employee> employeeRepo) {
         this.employeeRepo = employeeRepo;
+    }
+
+    private int contains(List<Pair<Integer, Integer>> list, int target) {
+
+        for (Pair<Integer, Integer> p : list) {
+            if (p.getValue0() == target) {
+                return list.indexOf(p);
+            }
+        }
+
+        return -1;
+    }
+
+    private int containsForTriplets(List<Triplet<Integer, Integer, Integer>> list, int target) {
+
+        for (Triplet<Integer, Integer, Integer> p : list) {
+            if (p.getValue0() == target) {
+                return list.indexOf(p);
+            }
+        }
+
+        return -1;
     }
 }
